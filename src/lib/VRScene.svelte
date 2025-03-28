98c7ca81697fdc7d476f24d3f9a7e98c9f1404a2
Laurie <laurie.in.japan@gmail.com>
2025-03-28 21:22:36 +0900

    make it smaller

--- src/lib/VRScene.svelte
+++ src/lib/VRScene.svelte
@@ -29,37 +29,6 @@
 />
 
 <T.DirectionalLight position={[0, 10, 10]} castShadow />
-
-<T.Mesh
-	rotation.y={rotation}
-	position.y={1}
-	scale={scale.current}
-	onpointerenter={() => {
-		scale.target = 1.5;
-	}}
-	onpointerleave={() => {
-		scale.target = 1;
-	}}
-	castShadow
->
-	<T.BoxGeometry args={[1, 2, 1]} />
-	<T.MeshStandardMaterial color="hotpink" />
-</T.Mesh>
-
-<T.Mesh rotation.x={-Math.PI / 2} receiveShadow>
-	<T.CircleGeometry args={[4, 40]} />
-	<T.MeshStandardMaterial color="white" />
-</T.Mesh>
-
-<T.PerspectiveCamera
-	makeDefault
-	position={[10, 10, 10]}
-	oncreate={(ref) => {
-		ref.lookAt(0, 1, 0);
-	}}
-/>
-
-<T.DirectionalLight position={[0, 10, 10]} castShadow />
 <T.AmbientLight />
 
 <T.Mesh
@@ -67,14 +36,14 @@
 	position.y={1}
 	scale={scale.current}
 	onpointerenter={() => {
-		scale.target = 1.5;
+		scale.target = 1.2;
 	}}
 	onpointerleave={() => {
 		scale.target = 1;
 	}}
 	castShadow
 >
-	<T.BoxGeometry args={[1, 2, 1]} />
+	<T.BoxGeometry args={[0.2, 0.6, 0.2]} />
 	<T.MeshStandardMaterial color="hotpink" />
 </T.Mesh>
 
