9334a04661cfa2aa23aff3383594ec712657d702
Laurie <laurie.in.japan@gmail.com>
2025-04-12 18:43:40 +0900

    base path

--- svelte.config.js
+++ svelte.config.js
@@ -8,13 +8,14 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		paths: {
-			// deploying to github pages
-			assets: process.env.NODE_ENV === 'production' ? '/webvr-template' : '',
-
-			relative: false
+			assets:
+				process.env.NODE_ENV === 'production'
+					? 'https://lauriegriffiths.github.io/webvr-template'
+					: '',
+			base: process.env.NODE_ENV === 'production' ? '/webvr-template' : ''
 		},
+		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
