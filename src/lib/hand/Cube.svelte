581d46d164077185805a5d7214987ba3dae5358b
Laurie <laurie.in.japan@gmail.com>
2025-04-12 17:51:28 +0900

    take away instancing

--- src/lib/hand/Cube.svelte
+++ src/lib/hand/Cube.svelte
@@ -10,23 +10,27 @@
 </script>
 
 <T.Group position={[0, 1.7, 0]}>
-	<InstancedMesh {limit}>
-		{#each { length: limit } as _, index (index)}
-			{#if colors[index] === 'hotpink'}
-				<RigidBody onsensorenter={() => (colors[index] = 'blue')}>
-					<Collider shape="cuboid" args={[size / 2, size / 2, size / 2]} />
-					<Instance color="hotpink" />
+	{#each { length: limit } as _, index (index)}
+		{#if colors[index] === 'hotpink'}
+			<RigidBody onsensorenter={() => (colors[index] = 'blue')}>
+				<Collider shape="cuboid" args={[size / 2, size / 2, size / 2]} />
+				<T.Mesh>
 					<T.BoxGeometry args={[size, size, size]} />
 					<T.MeshStandardMaterial roughness={0} metalness={0.2} color={colors[index]} />
-				</RigidBody>
-			{:else}
-				<RigidBody>
-					<Collider shape="cuboid" args={[size / 2, size / 2, size / 2]} />
-					<Instance color="blue" />
+				</T.Mesh>
+			</RigidBody>
+		{/if}
+	{/each}
+	{#each { length: limit } as _, index (index)}
+		{#if colors[index] === 'blue'}
+			<RigidBody>
+				<Collider shape="cuboid" args={[size / 2, size / 2, size / 2]} />
+
+				<T.Mesh>
 					<T.BoxGeometry args={[size, size, size]} />
 					<T.MeshStandardMaterial roughness={0} metalness={0.2} color={colors[index]} />
-				</RigidBody>
-			{/if}
-		{/each}
-	</InstancedMesh>
+				</T.Mesh>
+			</RigidBody>
+		{/if}
+	{/each}
 </T.Group>
