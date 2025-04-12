e1c196b245ffc59239834cc164b244482838c378
Laurie <laurie.in.japan@gmail.com>
2025-04-12 17:54:57 +0900

    simplify

--- src/lib/hand/Cube.svelte
+++ src/lib/hand/Cube.svelte
@@ -11,26 +11,12 @@
 
 <T.Group position={[0, 1.7, 0]}>
 	{#each { length: limit } as _, index (index)}
-		{#if colors[index] === 'hotpink'}
-			<RigidBody onsensorenter={() => (colors[index] = 'blue')}>
-				<Collider shape="cuboid" args={[size / 2, size / 2, size / 2]} />
-				<T.Mesh>
-					<T.BoxGeometry args={[size, size, size]} />
-					<T.MeshStandardMaterial roughness={0} metalness={0.2} color={colors[index]} />
-				</T.Mesh>
-			</RigidBody>
-		{/if}
-	{/each}
-	{#each { length: limit } as _, index (index)}
-		{#if colors[index] === 'blue'}
-			<RigidBody>
-				<Collider shape="cuboid" args={[size / 2, size / 2, size / 2]} />
-
-				<T.Mesh>
-					<T.BoxGeometry args={[size, size, size]} />
-					<T.MeshStandardMaterial roughness={0} metalness={0.2} color={colors[index]} />
-				</T.Mesh>
-			</RigidBody>
-		{/if}
+		<RigidBody onsensorenter={() => (colors[index] = 'blue')}>
+			<Collider shape="cuboid" args={[size / 2, size / 2, size / 2]} />
+			<T.Mesh>
+				<T.BoxGeometry args={[size, size, size]} />
+				<T.MeshStandardMaterial roughness={0} metalness={0.2} color={colors[index]} />
+			</T.Mesh>
+		</RigidBody>
 	{/each}
 </T.Group>
