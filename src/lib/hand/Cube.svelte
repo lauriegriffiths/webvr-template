4cffd2b68dfc60b158f4e22de606102b35515942
Laurie <laurie.in.japan@gmail.com>
2025-04-12 17:39:58 +0900

    colors

--- src/lib/hand/Cube.svelte
+++ src/lib/hand/Cube.svelte
@@ -5,19 +5,28 @@
 
 	const size = 0.02;
 	const limit = 100;
-	let color = $state('hotpink');
+	const startColor = 'hotpink';
+	let colors = $state(Array(limit).fill(startColor));
 </script>
 
 <T.Group position={[0, 1.7, 0]}>
 	<InstancedMesh {limit}>
-		<T.BoxGeometry args={[size, size, size]} />
-		<T.MeshStandardMaterial roughness={0} metalness={0.2} {color} />
-
 		{#each { length: limit } as _, index (index)}
-			<RigidBody onsensorenter={() => (color = 'blue')}>
-				<Collider shape="cuboid" args={[size / 2, size / 2, size / 2]} />
-				<Instance color="hotpink" />
-			</RigidBody>
+			{#if colors[index] === 'hotpink'}
+				<RigidBody onsensorenter={() => (colors[index] = 'blue')}>
+					<Collider shape="cuboid" args={[size / 2, size / 2, size / 2]} />
+					<Instance color="hotpink" />
+					<T.BoxGeometry args={[size, size, size]} />
+					<T.MeshStandardMaterial roughness={0} metalness={0.2} color={colors[index]} />
+				</RigidBody>
+			{:else}
+				<RigidBody>
+					<T.BoxGeometry args={[size, size, size]} />
+					<T.MeshStandardMaterial roughness={0} metalness={0.2} color={colors[index]} />
+					<Collider shape="cuboid" args={[size / 2, size / 2, size / 2]} />
+					<Instance color="blue" />
+				</RigidBody>
+			{/if}
 		{/each}
 	</InstancedMesh>
 </T.Group>
